81959a27059d45f888cf4d39df8ad6e780026017
aaronwai <aaron.lung.wai@gmail.com>
2025-08-08 17:46:49 +0800

    refactor Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div class='hero'>
+      <div class='container'>
+        <h1>continue exploring</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <a href='#' class='btn'>
+          explore tours
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
