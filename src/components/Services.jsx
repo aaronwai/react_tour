edc427fb6bc299ff21bb90150f3e841ab358037d
aaronwai <aaron.lung.wai@gmail.com>
2025-08-12 10:05:11 +0800

    final

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,33 +1,14 @@
 import Title from "./Title";
+import { services } from "../data";
+import Service from "./Service";
 const Services = () => {
   return (
     <main className='services' id='services'>
       <Title title='our' subtitle='services' />
       <div className='services-content'>
-        <div className='service-title'>
-          <i className='fa-solid fa-wallet'></i>
-          <h3>saving money</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            assumenda.
-          </p>
-        </div>
-        <div className='service-title'>
-          <i className='fa-solid fa-tree'></i>
-          <h3>endless hiking</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perspiciatis, eius.
-          </p>
-        </div>
-        <div className='service-title'>
-          <i className='fa-solid fa-socks'></i>
-          <h3>amazing comfort</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            provident saepe laborum quibusdam corrupti recusandae numquam, iure
-          </p>
-        </div>
+        {services.map((service) => (
+          <Service key={service.id} {...service} />
+        ))}
       </div>
     </main>
   );
